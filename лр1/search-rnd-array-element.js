3e7012f3e9c32c9a8f21395056b0955914384e1d
Elena-Chuprina <31988212+Elena-Chuprina@users.noreply.github.com>
2017-09-24 19:28:15 +0300

    Update search-rnd-array-element.js

--- лр1/search-rnd-array-element.js
+++ лр1/search-rnd-array-element.js
@@ -1,1 +1,20 @@
+var
+  a = [];
+  isFound = False;
+  i = 0;
+while(a.length<10) 
+    {b = Math.floor(Math.random() * 15);
+      a.push(b);}
+ alert('Filled random array: ');
+ for (i = 0; i < 10; i++)
+    {alert(a[i], ' ');}  
+ k = promt('Check number!', 1);
+ for (i = 0; i < 10; i++)
+     {if a[i] == k then 
+        {IsFound = True;
+          break;} }
+ if (IsFound==True) then
+     {alert('Element ', k, ' is found!');}  
+  else
+     {alert('Element ', k, ' not found');} 
 
