d0cc8e87062ebf819049aab4cd1c3fc442204471
Elena-Chuprina <31988212+Elena-Chuprina@users.noreply.github.com>
2017-09-28 19:51:11 +0300

    Update search-rnd-array-element.js

--- лр1/search-rnd-array-element.js
+++ лр1/search-rnd-array-element.js
@@ -1,19 +1,19 @@
 var
   a = [];
   i = 0;
-  IsFound = False;
 while(a.length<10) 
     {b = Math.floor(Math.random() * 15);
       a.push(b);}
  k = prompt('Введите число:', 1);
  alert('Случайный массив: ');
  for (i = 0; i < 10; i++)
-    {alert(a[i], ' ');}  
+    {alert(a[i], ' ');} 
+ var IsFound = true;
  for (i = 0; i < 10; i++)
      {if (a[i] == k)
-        {IsFound = True;
+        {IsFound = true;
           break;} }
- if (IsFound==True)
+ if (IsFound==false)
      {alert('Элемент ', k, ' найден!');}  
  else
      {alert('Элемент ', k, ' не найден');} 
