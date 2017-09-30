39604afe21987e8507eb896166994877cef335c6
Elena-Chuprina <31988212+Elena-Chuprina@users.noreply.github.com>
2017-09-30 13:19:28 +0300

    Update search-rnd-array-elements.js

--- лр1/js/search-rnd-array-elements.js
+++ лр1/js/search-rnd-array-elements.js
@@ -1,12 +1,12 @@
-﻿function search() { 
+function search() { 
   var arr = [];
   var size = 0;
   while (arr.length<10) {
     arr.push(Math.floor(Math.random() * 15));
   }
-  num = prompt('Введите число:', 1);
+  var num = prompt('Введите число:', 1);
   alert('Случайный массив: '+ arr.join (' '));
-  var IsFound = false;
+  var isFound = false;
   for (size = 0; size < 10; size++) {
     if (arr[size] == num) {
       isFound = true;
