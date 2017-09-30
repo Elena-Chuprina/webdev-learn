5c17c9ff1705c4087529b25e683d29bd22462342
Elena-Chuprina <31988212+Elena-Chuprina@users.noreply.github.com>
2017-09-30 12:56:45 +0300

    Update search-rnd-array-element.js

--- лр1/search-rnd-array-element.js
+++ лр1/search-rnd-array-element.js
@@ -2,20 +2,20 @@ function search() {
   var arr = [];
   var size = 0;
   while (arr.length<10) {
-      arr.push(Math.floor(Math.random() * 15));
+    arr.push(Math.floor(Math.random() * 15));
   }
   num = prompt('Введите число:', 1);
   alert('Случайный массив: '+ arr.join (' '));
   var IsFound = false;
   for (size = 0; size < 10; size++) {
-      if (arr[size] == num) {
-        isFound = true;
-        break;
-      } 
+    if (arr[size] == num) {
+      isFound = true;
+      break;
+    } 
   }
   if (isFound == true) {
-      alert('Элемент '+ num + ' найден!');
+    alert('Элемент '+ num + ' найден!');
   } else {
-      alert('Элемент '+ num + ' не найден');
+    alert('Элемент '+ num + ' не найден');
   } 
 }
